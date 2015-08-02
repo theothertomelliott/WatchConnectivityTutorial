b468819ab4d7e6b3941a07c047fc18a0479190ba
Thomas Elliott <tom.w.elliott@gmail.com>
2015-08-02 01:20:31 -0400

    Step 1: Create a new iOS project with a WatchKit App

---
+++ Swift/WatchConnectivityExample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  WatchConnectivityExample
+//
+//  Created by Thomas Elliott on 8/2/15.
+//  Copyright © 2015 Tom Elliott. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
