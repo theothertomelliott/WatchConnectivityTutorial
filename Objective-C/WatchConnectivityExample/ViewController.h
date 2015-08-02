72bc39c4307bd9e06008f2ae09c01de2a79dbc6b
Thomas Elliott <tom.w.elliott@gmail.com>
2015-08-02 01:32:34 -0400

    Step 3: Add a label and text field to the iOS app UIView and attach an outlet and action for the text field to the UIViewController.

--- Objective-C/WatchConnectivityExample/ViewController.h
+++ Objective-C/WatchConnectivityExample/ViewController.h
@@ -10,6 +10,9 @@
 
 @interface ViewController : UIViewController
 
+@property (weak, nonatomic) IBOutlet UITextField *messageField;
+
+- (IBAction)messageChanged:(id)sender;
 
 @end
 
--- Objective-C/WatchConnectivityExample/ViewController.m
+++ Objective-C/WatchConnectivityExample/ViewController.m
@@ -24,4 +24,7 @@
     // Dispose of any resources that can be recreated.
 }
 
+- (IBAction)messageChanged:(id)sender {
+}
+
 @end
