da433386ea3477d82e6bf0af069d22365923ccd4
Thomas Elliott <tom.w.elliott@gmail.com>
2015-08-02 01:32:34 -0400

    Step 3: Add a label and text field to the iOS app UIView and attach an outlet and action for the text field to the UIViewController.

--- Swift/WatchConnectivityExample/ViewController.swift
+++ Swift/WatchConnectivityExample/ViewController.swift
@@ -10,6 +10,11 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var messageField: UITextField!
+    
+    @IBAction func messageChanged(sender: AnyObject) {
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
